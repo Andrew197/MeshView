614843627b22daebf5fb36e14c0bf23ece6b75a7
Andrew <mobius01@gmail.com>
2014-11-18 19:00:01 -0600

    start page performance improvements

--- index.php
+++ index.php
@@ -3,12 +3,6 @@
 <head>
 <title>MeshView Start</title>
 
-<!-- dependencies -->
-<script type="text/javascript" src="jquery-2.1.0.min.js"></script>
-
-<!-- Meshview Program -->
-<link rel="stylesheet" href="animate.css">
-
 <style>
     body, html 
     { 
@@ -26,7 +20,9 @@
         width: 100%;
         height: 100%;
         z-index:0;
+
     }
+
     #overlay
     {
     	position:absolute;
@@ -47,13 +43,63 @@
 
     #footer 
     {
-    position: fixed; 
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 25px;
-    margin-left:10px;
-    color:gray;
+        position: fixed; 
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 25px;
+        margin-left:10px;
+        color:gray;
+    }
+
+    .slide
+    {
+      -webkit-animation-duration: 1s;
+      animation-duration: 1s;
+      
+      -webkit-animation-fill-mode: both;
+      animation-fill-mode: both;
+    }
+
+@-webkit-keyframes slideDown 
+{
+  0% 
+  {
+    opacity: 0;
+    -webkit-transform: translateY(-60px);
+    transform: translateY(-60px);
+  }
+  100% 
+  {
+    opacity: 1;
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+  }
+}
+
+@keyframes slideDown 
+{
+  0% 
+  {
+    opacity: 0;
+    -webkit-transform: translateY(-20px);
+    -ms-transform: translateY(-20px);
+    transform: translateY(-20px);
+  }
+  100% 
+  {
+    opacity: 1;
+    -webkit-transform: translateY(0);
+    -ms-transform: translateY(0);
+    transform: translateY(0);
+  }
+}
+
+
+.slideDown 
+{
+  -webkit-animation-name: slideDown;
+  animation-name: slideDown;
 }
 
 a:link {color:gray;}      /* unvisited link */
@@ -77,7 +123,7 @@ error_reporting(0);
 
 <body bgcolor="#000000">
 	<div id="overlay">
-        <img src="logo.png" class="animated fadeInDown"><br>
+        <img src="logo.png" class="slide slideDown"><br>
 		<form action="meshview.php" method="post">
 			<input type="text" id="urlbox" name="model" value="<?php file_put_contents('tmp.jpg', file_get_contents($_POST['model'])); if ($_POST["model"] == "") echo "cat.obj"; else echo $_POST["model"]; ?>">
             <input type="hidden" name="dummy" value="something">
